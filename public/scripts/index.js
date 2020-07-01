2ea4145cbd6b5bf9ad057f537099eec5a2476dca
Pierre Dias <pierrecamposdias@hotmail.com>
2020-07-01 11:38:27 -0300

    Fechar tela de pesquisa pelo botão ESC

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,7 +1,16 @@
 const buttonSearch = document.querySelector("#page-home main a");
 const modal = document.querySelector("#modal");
+const modalInput = document.querySelector("#modal input");
 const close = document.querySelector("#modal .header a");
 
-buttonSearch.addEventListener("click", () => modal.classList.remove("hide"));
+buttonSearch.addEventListener("click", () => {
+    modal.classList.remove("hide");    
+    modalInput.focus();
+});
+
+//Fechar o modal pela tecla ESC do teclado
+modal.addEventListener("keyup", (e) => {    
+    if(e.keyCode == 27) modal.classList.add("hide");
+});
 
 close.addEventListener("click", () => modal.classList.add("hide"));
